19d625afc3e823a1e045c7755e8ac7cc75a803e5
mochoi42 <principle950@naver.com>
2024-09-19 09:19:55 +0900

    최초

---
+++ kosta01/src/main/java/com/example/demo/Kosta01Application.java
@@ -0,0 +1,12 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Kosta01Application {
+	
+	public static void main(String[] args) {
+		SpringApplication.run(Kosta01Application.class, args);
+	}
+}
